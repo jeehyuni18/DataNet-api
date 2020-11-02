df7efc30dd6d2032abd356a354d8490f2a4523b4
jeehyuni18 <jeehyuni18@illunex.com>
2020-11-02 12:40:55 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3020
+const cors = require('cors');
+
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({host: process.env.DB_HOST, port: 3306, password: process.env.DB_PASS , user: process.env.DB_USER, connectionLimit: 5});
+
+app.use(cors());
+
+app.get('/api/company', (req, res) => {
+    pool.getConnection()
+        .then(conn => {
+            conn.query("SELECT * FROM datanet.nodeTemp")
+                .then(rows=>  { // rows: [ {val: 1}, meta: ... ]
+                    res.statusCode = 200
+                    res.send({ rows })
+                    conn.release();
+                })
+                .catch(err => {
+                    console.log(err)
+                    conn.release(); // release to pool
+                })
+        }).catch(err => {
+        console.log(err)
+    });
+})
+
+app.get('/api/company/link', (req, res) => {
+    pool.getConnection()
+        .then(conn => {
+            conn.query("SELECT * FROM datanet.link")
+                .then(rows=>  { // rows: [ {val: 1}, meta: ... ]
+                    res.statusCode = 200
+                    res.send({ links:rows })
+                    conn.release();
+                })
+                .catch(err => {
+                    console.log(err)
+                    conn.release(); // release to pool
+                })
+        }).catch(err => {
+        console.log(err)
+    });
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
